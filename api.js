5b2fc04c48b83d408871ca4edbc0463c79bd87e3
hectorslqm <hectorslqm@gmail.com>
2025-01-16 11:19:11 -0600

    adding different endpoints

--- api.js
+++ api.js
@@ -6,6 +6,23 @@ app.get('/', (req, res) => {
   res.status(200).send('Authentication API')
 })
 
+app.post('/', (req, res) => {
+  res.status(201).send('Created')
+})
+
+app.put('/:id', (req, res) => {
+  console.log(req.params)
+  res.sendStatus(204)// No Content just the status code
+})
+
+app.patch('/:id', (req, res) => {
+  res.sendStatus(204)// No Content just the status code
+})
+
+app.delete('/:id', (req, res) => {
+  res.sendStatus(204)// No Content just the status code
+})
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`)
 })
